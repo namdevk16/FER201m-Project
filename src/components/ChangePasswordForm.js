633b77008ee806e97d903d08f444362f30ccaff9
tuanpmhe161621 <tuanpmhe161621@fpt.edu.vn>
2023-07-10 19:12:50 +0700

    abc

--- src/components/ChangePasswordForm.js
+++ src/components/ChangePasswordForm.js
@@ -1,25 +1,47 @@
-
+import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import { Container, Row } from 'react-bootstrap';
 
 const ChangePasswordForm = () => {
     return (
-        <div className="change-password">
-            <div>
-                <h3>Did you forgot your password?</h3>
+        <div className='signup-form'>
+            <Container className="my-5 gradient-form">
 
-                <p>Back to login</p>
+                <Row>
+                    <div className='col-lg-6 col-md-12 col-sm-12 p-left'>
+                        <div className="d-flex flex-column  justify-content-center gradient-custom-2 h-100 mb-4">
 
-                <button>Login</button>
-            </div>
+                            <div className="text-white px-3 py-4 p-md-5 mx-md-4">
+                                <h4 class="mb-4">Did you forgot your password?</h4>
+                                <p class="small mb-0">Back to Login</p>
+                            </div>
 
-            <div>
-                <h3>Reset password</h3>
-                <form>
-                    <input type="text" name="email" placeholder="Enter your email"/>
-                    <input type="password" name="password" placeholder="Enter your new password"/>
-                    <input type="password" name="confirm_password" placeholder="Confirm your new password"/>
-                    <button>Reset</button>
-                </form>
-            </div>
+                        </div>
+
+                    </div>
+
+                    <form className='col-lg-6 col-md-12 col-sm-12 p-right' >
+                        <div className="d-flex flex-column ms-5">
+                            <h3>Change password</h3>
+                            <div className="input">
+                                <label htmlFor='email'>Email</label>
+                                <input id='email' type='text' className="form-control"/>
+                            </div>
+                            <div className="input">
+                                <label htmlFor='newpass'>New password</label>
+                                <input id='newpass' type='text' className="form-control"/>
+                            </div>
+                            <div className="input">
+                                <label htmlFor='confirmpass'>Confirm password</label>
+                                <input id='confirmpass' type='text' className="form-control"/>
+                            </div>  
+                        </div>
+
+                    </form>
+
+                </Row>
+
+            </Container>
         </div>
     );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import { NavLink, Link, useNavigate } from 'react-router-dom';
-import { Container, Nav, Navbar } from 'react-bootstrap'
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
 
 
 const Header = () => {
@@ -27,15 +27,21 @@ const Header = () => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand><NavLink to={'/'}><img style={{height:'56px'}} src='https://admin.googleusercontent.com/logo-scs-key2294502' alt='#'/></NavLink></Navbar.Brand>
+                <Navbar.Brand><NavLink to={'/'}><img style={{ height: '56px' }} src='https://admin.googleusercontent.com/logo-scs-key2294502' alt='#' /></NavLink></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link as={NavLink} to={'/'}>Home</Nav.Link>
                         <Nav.Link as={NavLink} to={'/aboutus'}>About us</Nav.Link>
                         <Nav.Link as={NavLink} to={'/post'} onClick={(e) => handleCheckLogin(e)}>Post</Nav.Link>
-                        {!sessionStorage.getItem('account') ? <Nav.Link as={NavLink} to={'/login'}>Login</Nav.Link> : <Nav.Link as={NavLink} to={'/login'}>{JSON.parse(sessionStorage.getItem('account')).fullname}</Nav.Link>}
-                        {!sessionStorage.getItem('account') ? <Nav.Link as={NavLink} to={'/signup'}>Sigup</Nav.Link> : <Nav.Link onClick={handleLogout}>Signout</Nav.Link>}
+                        {
+                            !sessionStorage.getItem('account') ? <Nav.Link as={NavLink} to={'/login'}>Login</Nav.Link> :
+                                <NavDropdown title={JSON.parse(sessionStorage.getItem('account')).fullname} id="basic-nav-dropdown">
+                                    <NavDropdown.Item href="#action/3.2">Trang cá nhân</NavDropdown.Item>
+                                    <NavDropdown.Item as={NavLink} to={'/changepassword'}>Thay đổi mật khẩu</NavDropdown.Item>
+                                </NavDropdown>
+                        }
+                        {!sessionStorage.getItem('account') ? <Nav.Link as={NavLink} to={'/signup'}>Sigup</Nav.Link> : <Nav.Link onClick={handleLogout}>Logout</Nav.Link>}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
