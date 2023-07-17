30f59eee2cd696d6ad68fe3f1cfd7e41d0e5b5ca
namnhhe161918 <namnhhe161918@fpt.edu.vn>
2023-07-17 16:04:18 +0700

    updateDashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,13 +3,16 @@ import '../styles/custom.css'
 import SideBar from './SideBar';
 import HeaderDashboard from './HeaderDashboard';
 import { useEffect, useState } from 'react';
+import Chart from 'react-apexcharts';
 const Dashboard = () => {
     const [userCount, setUserCount] = useState(0);
     const [dormCount, setDormCount] = useState(0);
     const [hotelCount, setHotelCount] = useState(0);
     const [houseCount, setHouseCount] = useState(0);
     const [vertifyCount, setVertifyCount] = useState(0);
-
+    const [regions, setRegions] = useState([]);
+    const [houses, setHouses] = useState([]);
+    const [quantity, setQuantity] = useState([])
     useEffect(() => {
         const fetchUserCount = async () => {
             try {
@@ -33,17 +36,17 @@ const Dashboard = () => {
                 let dormCounter = 0;
                 let houseCounter = 0;
                 let hotelCounter = 0;
-        
+
                 data.forEach(ho => {
-                  if (ho.category_id === 1) {
-                    dormCounter++;
-                  } else if (ho.category_id === 2) {
-                    houseCounter++;
-                  } else if (ho.category_id === 3) {
-                    hotelCounter++;
-                  } 
+                    if (ho.category_id === 1) {
+                        dormCounter++;
+                    } else if (ho.category_id === 2) {
+                        houseCounter++;
+                    } else if (ho.category_id === 3) {
+                        hotelCounter++;
+                    }
                 });
-        
+
                 setDormCount(dormCounter);
                 setHotelCount(houseCounter);
                 setHouseCount(hotelCounter);
@@ -70,6 +73,97 @@ const Dashboard = () => {
         fetchUserCount();
     }, []);
 
+
+    useEffect(() => {
+        const fetchRegion = async () => {
+            try {
+                const response = await fetch('http://localhost:9999/region');
+                const data = await response.json();
+                setRegions(data);
+            } catch (error) {
+                console.error('Error fetching user count:', error);
+            }
+        };
+
+        fetchRegion();
+    }, []);
+
+    // useEffect(() => {
+    //     Promise.all([
+    //         fetch(`http://localhost:9999/houseInformation`),
+    //         fetch(`http://localhost:9999/region`)
+    //     ])
+    //         .then(res => res.json())
+    //         .then((data) => {
+    //             const house = data[0];
+    //             const re = data[1];
+    //             let arr = []
+    //             for (var i = 0; i < house.length; i++) {
+    //                 let count = 0;
+    //                 for (var j = 0; j < re.length; j++) {
+    //                     if (house[j].address_id === re[i].id) {
+    //                         count++;
+    //                     }
+    //                 }
+    //                 arr.push({
+    //                     reg_id: re[i].id,
+    //                     count: count
+    //                 })
+    //             }
+    //             setQuantity(arr)
+    //         })
+    //         .catch(err => {
+    //             console.log(err.message);
+    //         });
+    // }, [])
+
+    // console.log(quantity);
+
+
+    const options = {
+        chart: {
+            id: "basic-bar"
+        },
+        xaxis: {
+            categories: ['Thị trấn Liên Quan','Xã Bình Phú','Xã Bình Yên','Xã Cẩm Yên','Xã Cần Kiệm','Xã Canh Nậu',
+                         'Xã Chàng Sơn','Xã Đại Đồng','Xã Dị Nậu','Xã Đồng Trúc','Xã Hạ Bằng','Xã Hương Ngải','Xã Hữu Bằng','Xã Kim Quan',
+                         'Xã Lại Thượng','Xã Phú Kim','Xã Phùng Xá','Xã Tân Xã','Xã Thạch Hoà','Xã Thạch Xá','Xã Tiến Xuân','Xã Yên Bình','Xã Yên Trung']
+            // categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+        }
+    };
+
+    const series = [
+        {
+            name: "series-1",
+            data: [0,0,0,0,0,0,0,0,0,1,3,0,0,0,0,0,0,2,14,0,0,0,0]
+            // data: [30, 40, 45, 50, 49, 60, 70, 91]
+        }
+    ];
+
+    useEffect(() => {
+        console.log("Handle quantity:");
+        console.log(quantity);
+        console.log(regions);
+        console.log(houses);
+        let arr = []
+        for (var i = 0; i < regions.length; i++) {
+            let count = 0;
+            for (var j = 0; j < houses.length; j++) {
+                if (houses[j].address_id === regions[i].id) {
+                    count++;
+                }
+            }
+            arr.push({
+                reg_id: regions[i].id,
+                count: count
+            })
+        }
+        setQuantity(arr)
+    }, [])
+
+    // console.log(quantity);
+    // console.log(regions);
+    // console.log(houses);
     return (
         <>
             <SideBar />
@@ -146,10 +240,9 @@ const Dashboard = () => {
                                 </div>
                                 </div>
                             </div>
-                            <h5 className="h5 fw-normal">Sales marketing report</h5>
-                            <p className="text-sm text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor amet
-                                officiis</p>
-                            <canvas id="lineCahrt" style={{ display: 'block', height: '299px', width: '600px' }} width="750" height="373" className="chartjs-render-monitor"></canvas>
+                            <h5 className="h5 fw-normal">Thống Kê Các Xã</h5>
+                            <p className="text-sm text-muted">Bao gồm 23 xã trong khu vực Sơn Tây</p>
+                            <Chart options={options} series={series} type="bar" width="100%" />
                         </div>
                     </div>
                 </div>
